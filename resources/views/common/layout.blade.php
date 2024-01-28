2aa484273d15867390319e27d7e0d280f89dc88f
mecnun-kilinc <info@ankaranetwork.com>
2024-01-28 03:05:53 +0300

    added some css classes

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -8,44 +8,68 @@
     <meta  name="description" content="{{ $metaDescription }}" >
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('/catalog/css/custom.css') }}">
+
 </head>
 <body>
-   <header class="text-white pt-2 pb-4">
-    <nav class="navbar navbar-expand-lg">
-        <div class="container">
-          <a class="navbar-brand text-black" href="{{ URL('/') }}">Logo</a>
-          <div class="text-end">
-            <ul class="navbar-nav">
-                @guest
-                    <li class="nav-item">
-                        <a class="btn btn-outline-primary me-2 {{ (request()->is('login')) ? 'active' : '' }}" href="{{ route('login') }}">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="btn btn-outline-dark {{ (request()->is('register')) ? 'active' : '' }}" href="{{ route('register') }}">Register</a>
-                    </li>
-                @else  
+   <header class="text-white">
+    
+    <div class="container">
+   
+        <div class="header-menu">
 
-                     <li class="nav-item">
-                        <a class="btn btn-primary me-2" href="{{ route('dashboard') }}">
-                            {{ Auth::user()->name }}
-                        </a>
-                       </li>
-
-                        <li class="nav-item">
-                            <a class="btn btn-warning" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();"
-                            >Logout</a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                                @csrf
-                            </form>
-                        </li>
-
-                @endguest
-            </ul>
-          </div>
+        <div class="logo">
+            <a href="{{ URL('/') }}">
+                <img src="{{ asset('images/logo.png') }}" class="img-fluid float-start" />
+            </a>          
         </div>
-    </nav>    
+      
+       <div class="arama">
+        <div class="input-group input-group-md">
+            <input type="text" name="search" class="form-control " placeholder="Search">	  
+            <div class="input-group-append">
+               <button type="button" class="btn btn-primary">Search
+               </button>
+           </div>
+        </div>    
+       </div>
+       
+       <div class="uye">
+        
+            @guest
+                
+            <a class="btn btn-outline-primary me-2 {{ (request()->is('login')) ? 'active' : '' }}" href="{{ route('login') }}">Login</a> 
+            <a class="btn btn-primary{{ (request()->is('register')) ? 'active' : '' }}" href="{{ route('register') }}">Register</a>
+                
+            @else  
+
+                
+                    <a class="btn btn-primary me-2" href="{{ route('dashboard') }}">
+                        {{ Auth::user()->name }}
+                    </a>
+                  
+
+                   
+                        <a class="btn btn-warning" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();"
+                        >Logout</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                            @csrf
+                        </form>
+                    
+
+            @endguest
+        </ul>     
+       </div>
+       
+      </div>  
+    
+ </div> 	
+
+
+
+ 
+
    </header>
 
 
